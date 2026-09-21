aec603ccefc502cecb67de48a9c873a575e5c365
PHANI NAGA MANIKANTA UPPALA <uppalamanikanta79@gmail.com>
2026-09-21 21:14:27 +0530

    Add Good Contest [A] from Codeforces

---
+++ 2266/A - Good Contest/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int a,b,c;
+        cin >> a >> b >> c;
+        int mini = min(a , min(b,c));
+        cout << n - mini << endl;
+    }
+}
